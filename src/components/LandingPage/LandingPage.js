17fe4ee50ff279a6b6e92a992fd6e56c330a1928
Sophia Koeut <koeutsophia@gmail.com>
2020-01-07 13:03:38 -0800

    added styling and media queries for Error page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,27 +6,53 @@ class App extends Component {
     render() {
         return (
             <div className="landing-page">
-                <h1 className="landing-page-header">Welcome Parents!</h1>
-                <p>Making a visual schedule for your child is time-consuming. Parents have places to go and do within the day. Time is precious.
-                    With this application, parents will have their child's schedule on the go on their mobile devices or at home on their personal commputer to create a visual schedule without
+
+                <h1 className="landing-page-header">Calling All Parents!</h1>
+                <p className="attention-grabber">Making a visual schedule for your child is time-consuming. Parents have places to go and do within the day. Time is precious.
+                    With this application, parents will have their child's schedule on the go on their mobile devices or at home on their personal computer to create a visual schedule without
                     the hassle. So put down that scissor and glue and let's tackle the day together! TALLY-HO!
                 </p>
-                <h3>
-                    Get Started Here!
+                <section className="step-one">
+                    <h3>1. Sign up/Login</h3>
+                    <p>Provide an email and password</p>
+                    <img></img>
+                </section>
+                <section className="step-two">
+                    <h3>2. Create a new task</h3>
+                    <p>Note: To add an image, use the Google Image search engine to select an image. </p>
+                    <img></img>
+                    <p>Right Click on the chosen image and click on 'Copy Image Address'</p>
+                    <img></img>
+                    <p>Now paste into the 'Add Image' field</p>
+                </section>
+                <section className="step-three">
+                    <h3>3. Check off a Task</h3>
+                    <p>Check off a Task by tapping/clicking the image</p>
+                    <img></img>
+                </section>
+                <section className="step-four">
+                    <h3>4. Edit Multiple Tasks</h3>
+                    <p>In edit mode, you can edit multiple tasks and save each of them</p>
+                    <img></img>
+                </section>
+                <section className="link-to-app">
+                    <h3>
+                        Get Started Here!
                 </h3>
-                <p>
-                    <Link to={`/register`}>
-                        Sign up
+                    <p>
+                        <Link to={`/register`}>
+                            Sign up
                         </Link>
-                </p>
-                <h3>
-                    Been here already?
+                    </p>
+                    <h3>
+                        Been here already?
                 </h3>
-                <p>
-                    <Link to={`/login`}>
-                        Login
+                    <p>
+                        <Link to={`/login`}>
+                            Login
                         </Link>
-                </p>
+                    </p>
+                </section>
             </div>
         )
     }
--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import './NotFoundPage.css'
+import child from '../../img/6057415565_b3ecdc5af8_b.jpg'
 
 export default class NotFoundPage extends Component {
   render() {
     return (
       <section className='NotFoundPage'>
         <h2 className='not-found-header'>You hit a 404 which is computer lingo for this page doesn't exist!</h2>
-        <p className='not-found-header'>Try going back to the page you were at before</p>
+        <img class="oh-no-child" src={child} alt="guilty child"></img>
+        <h3 className='not-found-header'>Try going back to the page you were at before</h3>
       </section>
     )
   }
