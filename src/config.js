fa4f3cbed2aee48eaba3e5b5ff5fa90b70b62540
Sophia Koeut <koeutsophia@gmail.com>
2020-01-07 22:59:34 -0800

    modified for deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+    API_ENDPOINT: 'https://sleepy-eyrie-04770.herokuapp.com/',
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
 }
