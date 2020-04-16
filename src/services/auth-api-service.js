540838f6246efa2c4666dac9dbebe0d0633ea693
Sophia Koeut <koeutsophia@gmail.com>
2020-04-16 16:47:41 -0700

    added a refresh token in tokenservice and imported jwt token

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -1,4 +1,5 @@
 import config from '../config'
+import TokenService from './token-service'
 
 const AuthApiService = {
     postLogin(credentials) {
@@ -22,6 +23,19 @@ const AuthApiService = {
                 'content-type': 'application/json',
             },
             body: JSON.stringify(user),
+        })
+            .then(res =>
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            )
+    },
+    refreshToken() {
+        return fetch(`${config.API_ENDPOINT}/auth/token`, {
+            method: 'PUT',
+            headers: {
+                'authorization': `Bearer ${TokenService.getAuthToken()}`,
+            },
         })
             .then(res =>
                 (!res.ok)
