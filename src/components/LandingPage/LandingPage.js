1b57d0b332248bab406d3051b0dfcccbf51f8536
Sophia Koeut <koeutsophia@gmail.com>
2020-04-17 18:28:52 -0700

    set up banner image and text cutout

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -8,16 +8,14 @@ class App extends Component {
     render() {
         return (
             <div className="landing-page">
+                <div className="img-container">
+                    <div className="banner-title">Tallyho!</div>
+                </div>
 
-                <h1 className="landing-page-header">Calling All Parents!</h1>
-                <p className="description paragraph-one">Making a visual schedule for your child is time-consuming. Parents have places to go and chores to do within the day. Time is precious.
-                       
-                </p>
-                <p className="description paragraph-two">With Tallyho!, parents will have their child's schedule easily accessible through their mobile device or at home on their personal computer without
-                    the hassle. Simple and quick signup with just an email and password to start the day. Add multiple tasks with a title and image and you're on your way!</p>
-                <p className="description paragraph-three">So put down that scissor and glue and let's tackle the day together!</p>
-                <h2 className="slogan">TALLYHO!</h2>
-                <img className="children-play" src={children} alt="children playing follow the leader"></img> 
+
+
+
+
                 <section className="link-to-app">
                     <h3>
                         Get Started Here!
